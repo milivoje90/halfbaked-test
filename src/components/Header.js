0f715075d31b1056313026af358c9b41215463fd
Milivoje Pualic <milivojepualic@Milivojes-MacBook-Air.local>
2021-04-03 19:33:38 +0200

    header and balance basics

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <div>
+            <h2>Task 1</h2>
+            <p>It would be best if you described daily profit for a fictional company.</p>
+        </div>
+    )
+}
