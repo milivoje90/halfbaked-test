84c4f953b0e7c27aa3f4689d1973f5019bf95268
Milivoje Pualic <milivojepualic@Milivojes-MacBook-Air.local>
2021-04-03 22:21:06 +0200

    App Reducer started. CSS fixing..

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react';
+
+import { GlobalContext } from '../context/GlobalState';
 
 export const TransactionList = () => {
+    const context = useContext(GlobalContext);
+
+    console.log(context)
+
     return (
         <>
             <h3>History</h3>
-            <ul>
-                <li className="transactionList">
-                    Cash -$400
+            <ul className="transactionList">
+                <li className="minus">
+                    Cash <span>-$400</span><button className="delete-btn">x</button>
                 </li>
             </ul>
         </>
