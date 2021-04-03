2534107db50ec9c18f78447eb8c33b4f22661ae4
Milivoje Pualic <milivojepualic@Milivojes-MacBook-Air.local>
2021-04-03 22:36:54 +0200

    Transaction created

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,19 +1,16 @@
 import React, { useContext } from 'react';
+import { Transaction } from './Transaction';
 
 import { GlobalContext } from '../context/GlobalState';
 
 export const TransactionList = () => {
-    const context = useContext(GlobalContext);
-
-    console.log(context)
+    const { transactions } = useContext(GlobalContext);
 
     return (
         <>
             <h3>History</h3>
             <ul className="transactionList">
-                <li className="minus">
-                    Cash <span>-$400</span><button className="delete-btn">x</button>
-                </li>
+                {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
             </ul>
         </>
     )
