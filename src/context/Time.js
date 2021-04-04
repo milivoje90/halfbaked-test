5cc618322e00f52f80cb93fcdadd9e92e686b5ec
Milivoje Pualic <milivojepualic@Milivojes-MacBook-Air.local>
2021-04-04 18:29:44 +0200

    checking the new buttons

--- src/context/Time.js
+++ src/context/Time.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="globalTime">
-      <div className='time-circle'>
-        <div className="time">
-          00:04
+    const [seconds, setSeconds] = useState(0);
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        console.log('seconds', seconds);
+    }, [seconds]);
+
+    
+    console.log('count', count);
+
+    return (
+        <div className="globalTime">
+            <button onClick={() => setSeconds(seconds + 1)}>incr seconds</button>
+            <button onClick={() => setCount(count + 1)}>incr count</button>
+            <div className='time-circle'>
+                <div className="time">
+                    00:04
+                </div>
+            </div>
+            <div className="buttons">
+                <button className="play-pause">
+                    Play
+                </button>
+                <button className="play-pause">
+                    Pause
+                </button>
+                <button className="reset">
+                    Reset
+                </button>
+            </div>
         </div>
-      </div>
-      <div className="buttons">
-        <button className="play-pause">
-          Play
-        </button>
-        <button className="play-pause">
-          Pause
-        </button>
-        <button className="reset">
-          Reset
-        </button>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
